8c19ec901a5c1f4f32556ed491c68ace03f80ffd
Vasilis Spilka <vasspilka@gmail.com>
2016-06-23 09:47:37 +0300

    hash instead of instance variables

--- lib/reportier/persister.rb
+++ lib/reportier/persister.rb
@@ -5,12 +5,14 @@ module Reportier
     end
 
     def initialize
+      @reporting_vars = {}
       _initialize_default_reporting_vars
     end
 
     def save(item)
-      create_accessor(item) unless (eval "@#{item}")
-      eval "@#{item} += 1"
+      item = item.to_sym
+      initialize_reporting_var(item) unless @reporting_vars[item]
+      @reporting_vars[item] += 1
     end
 
     def report
@@ -18,41 +20,33 @@ module Reportier
     end
 
     def to_json
-      "{#{to_hash.map { |k,v| "\"#{k.to_s}\": #{v.to_s}" }.flatten.join(",\n")}}"
+      "{#{to_hash.map { |k,v| "\"#{k}\": #{v}" }.flatten.join(",\n")}}"
+    end
+
+    def clear
+      initialize
     end
 
     private
 
     def attr_messages
-      reporting_vars.map do |var|
-        "#{var}: #{eval var.to_s}\n"
+      @reporting_vars.map do |key, val|
+        "#{key}: #{val}\n"
       end
     end
     
-    def create_accessor(name)
-      self.class.module_eval "attr_accessor :#{name}"
-      eval "@#{name} ||= 0"
+    def initialize_reporting_var(name)
+      @reporting_vars[name] = 0
     end
 
     def to_hash
-      Hash[ *reporting_vars.collect { |v| [ v.to_s, (eval v.to_s) ] }.flatten ]
-    end
-
-    def reporting_vars
-      instance_variables
-    end
-
-    def clear
-      reporting_vars.each do |var|
-        eval "#{var} = 0"
-      end
-      initialize
+      @reporting_vars
     end
 
     def _initialize_default_reporting_vars
       Default::REPORTING_VARS.each do |key, val|
         raise TypeError unless val.kind_of? Integer
-        eval "@#{Namer.new.name(key)} = #{val}"
+        @reporting_vars[key] = val
       end
     end
   end
